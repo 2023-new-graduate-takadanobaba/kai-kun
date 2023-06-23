dda0f016f954efdbfe4fafca808310a9b504b3b8
YKYUTARO <135583611+YKYUTARO@users.noreply.github.com>
2023-06-23 09:35:29 +0900

    Update suzi.java

--- todo/src/main/java/suzi.java
+++ todo/src/main/java/suzi.java
@@ -1,8 +1,19 @@
 package main.java;
 
-public class suzi {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Suzi {
+    public static void main(String[] args) throws Exception {
+        System.out.println("数字を入力してください");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str = reader.readLine();
+        try {
+            double number = Double.parseDouble(str);
+            System.out.println(number);
+        } catch (NumberFormatException e) {
+            System.out.println("数字以外が入力されました");
 
-
-
+        }
+    }
 }
