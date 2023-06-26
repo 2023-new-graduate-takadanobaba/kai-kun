f980382fda92d3b1e61f118e650d302ee423f3cc
YKYUTARO <135583611+YKYUTARO@users.noreply.github.com>
2023-06-26 13:21:03 +0900

    Create Tests.java

---
+++ todo/src/main/java/Tests.java
@@ -0,0 +1,14 @@
+package main.java;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Tests {
+
+
+    @Test
+
+    public void test() {
+        assertEquals("expe", "expe");
+
+        }}
