cad33468e61103f0084434a5ff66ef101c8a0647
YKYUTARO <135583611+YKYUTARO@users.noreply.github.com>
2023-06-27 09:23:37 +0900

    Update Tests.java

--- todo/src/main/java/Tests.java
+++ todo/src/main/java/Tests.java
@@ -1,14 +1,38 @@
 package main.java;
-import static org.junit.Assert.assertEquals;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class Tests {
 
-
     @Test
-
-    public void test() {
-        assertEquals("expe", "expe");
-
+    public void ボタンが押せるかどうかの確認() {
+        // 準備
+        VendingMachine sut = new VendingMachine();
+        // 実行
+        sut.push();
+        // 検証
+        boolean actual = sut.isPushed();
+        assertEquals(true, actual);
+    }
+        @Test
+    public void ボタンが押されていないことの確認() {
+        // 準備
+        VendingMachine sut = new VendingMachine();
+        // 実行
+        // 検証
+         boolean actual = sut.isPushed();
+        assertEquals(false, actual);
         }}
+//    @Test
+//     public void ボタンを押すとコーラがでる() {
+//         // 準備
+//         VendingMachine sut = new VendingMachine();
+//         // 実行
+//     sut.push();
+//         // 検証
+//         String actual = new 
+//         assertEquals(true, actual);
+//     }
+// }
+
